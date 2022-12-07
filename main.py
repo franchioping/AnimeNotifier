397a1f6b2e661b529c405069ee62a89daa59c39b
franchioping <franchioping@gmail.com>
2022-12-07 16:29:02 +0000

    fix file location

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import api.anime_api as ani_api
 import animeManager
+import os
 
 if __name__ == "__main__":
-    ani_m = animeManager.AnimeManager("anime.json")
+    ani_m = animeManager.AnimeManager(os.getcwd() + "/anime.json")
     ani_m.anime_list.append(ani_api.AnimeAPI.search("Rent-a-Girlfriend")[1])
     ani_m.write_data()
